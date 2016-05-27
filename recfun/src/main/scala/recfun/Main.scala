7816ba339c1667be9445f14f771028fc4d0eec14
Arialdo Martini <arialdomartini@gmail.com>
2016-05-27 19:19:48 +0200

    Exercise 1: solved

--- recfun/src/main/scala/recfun/Main.scala
+++ recfun/src/main/scala/recfun/Main.scala
@@ -13,7 +13,10 @@ object Main {
   /**
    * Exercise 1
    */
-    def pascal(c: Int, r: Int): Int = ???
+    def pascal(c: Int, r: Int): Int = {
+      
+      if(c == 0 || r == 1) 1 else pascal(c - 1, r) + pascal(c, r - 1)
+    }
   
   /**
    * Exercise 2
