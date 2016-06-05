d2c567571a0be838aee22fb0d6c7f51e4faa5873
Arialdo Martini <arialdomartini@gmail.com>
2016-06-05 20:58:44 +0200

    Code indentation

--- funsets/src/main/scala/funsets/FunSets.scala
+++ funsets/src/main/scala/funsets/FunSets.scala
@@ -19,31 +19,31 @@ object FunSets {
   /**
    * Returns the set of the one given element.
    */
-    def singletonSet(elem: Int): Set = (e: Int) => e == elem
+  def singletonSet(elem: Int): Set = (e: Int) => e == elem
   
 
   /**
    * Returns the union of the two given sets,
    * the sets of all elements that are in either `s` or `t`.
    */
-    def union(s: Set, t: Set): Set = (e: Int) => contains(s, e) || contains(t, e)
+  def union(s: Set, t: Set): Set = (e: Int) => contains(s, e) || contains(t, e)
 
   /**
    * Returns the intersection of the two given sets,
    * the set of all elements that are both in `s` and `t`.
    */
-    def intersect(s: Set, t: Set): Set = (e: Int) => contains(s, e) && contains(t, e)
+  def intersect(s: Set, t: Set): Set = (e: Int) => contains(s, e) && contains(t, e)
 
   /**
    * Returns the difference of the two given sets,
    * the set of all elements of `s` that are not in `t`.
    */
-    def diff(s: Set, t: Set): Set = (e: Int) => contains(s, e) && !contains(t, e)
+  def diff(s: Set, t: Set): Set = (e: Int) => contains(s, e) && !contains(t, e)
 
   /**
    * Returns the subset of `s` for which `p` holds.
    */
-    def filter(s: Set, p: Int => Boolean): Set = (e: Int) => contains(s, e) && p(e)  
+  def filter(s: Set, p: Int => Boolean): Set = (e: Int) => contains(s, e) && p(e)  
 
   /**
    * The bounds for `forall` and `exists` are +/- 1000.
@@ -64,13 +64,13 @@ object FunSets {
    * Returns whether there exists a bounded integer within `s`
    * that satisfies `p`.
    */
-    def exists(s: Set, p: Int => Boolean): Boolean = ! forall(s, not(p))
+  def exists(s: Set, p: Int => Boolean): Boolean = ! forall(s, not(p))
 
 
   /**
    * Returns a set transformed by applying `f` to each element of `s`.
    */
-    def map(s: Set, f: Int => Int): Set = e => exists(s, x => f(x) == e)
+  def map(s: Set, f: Int => Int): Set = e => exists(s, x => f(x) == e)
 
 
   /**
