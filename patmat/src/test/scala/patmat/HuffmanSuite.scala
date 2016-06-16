c17913116c2ac9b743ad067bf320edef56870fcc
Arialdo Martini <arialdomartini@gmail.com>
2016-06-16 08:28:55 +0200

    string2chars was already implemented

--- patmat/src/test/scala/patmat/HuffmanSuite.scala
+++ patmat/src/test/scala/patmat/HuffmanSuite.scala
@@ -40,13 +40,11 @@ class HuffmanSuite extends FunSuite {
     }
   }
 
+  test("string2chars(\"hello, world\")") {
+    assert(string2Chars("hello, world") === List('h', 'e', 'l', 'l', 'o', ',', ' ', 'w', 'o', 'r', 'l', 'd'))
+  }
+
   /*
-
-test("string2chars(\"hello, world\")") {
-assert(string2Chars("hello, world") === List('h', 'e', 'l', 'l', 'o', ',', ' ', 'w', 'o', 'r', 'l', 'd'))
-}
-
-
 test("makeOrderedLeafList for some frequency table") {
 assert(makeOrderedLeafList(List(('t', 2), ('e', 1), ('x', 3))) === List(Leaf('e',1), Leaf('t',2), Leaf('x',3)))
 }
