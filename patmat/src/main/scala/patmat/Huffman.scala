965aa7f6e1572f9c467f35edc3b6cde411c3186f
Arialdo Martini <arialdomartini@gmail.com>
2016-06-18 10:14:43 +0200

    Can build a CodeTable given a CodeTree

--- patmat/src/main/scala/patmat/Huffman.scala
+++ patmat/src/main/scala/patmat/Huffman.scala
@@ -276,7 +276,15 @@ object Huffman {
   * a valid code tree that can be represented as a code table. Using the code tables of the
   * sub-trees, think of how to build the code table for the entire tree.
   */
-  def convert(tree: CodeTree): CodeTable = ???
+  def convert(tree: CodeTree): CodeTable = {
+    def traverse(remaining: CodeTree, bitsSoFar: List[Bit]): CodeTable = {
+      tree match {
+        case Fork(left, right, chars, _) => mergeCodeTables(traverse(left, bitsSoFar), traverse(right, bitsSoFar))
+        case Leaf(char, _) => List((char, bitsSoFar))
+      }
+    }
+    traverse(tree, List())
+  }
   
   /**
   * This function takes two code tables and merges them into one. Depending on how you
