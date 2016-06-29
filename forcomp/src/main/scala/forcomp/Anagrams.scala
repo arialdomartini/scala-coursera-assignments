4fe1013642baa64c22a9915cc48eeaf7ac78adc3
Arialdo Martini <arialdomartini@gmail.com>
2016-06-29 08:24:48 +0200

    sentenceOccurrences joins the words and calculates the occurrences

--- forcomp/src/main/scala/forcomp/Anagrams.scala
+++ forcomp/src/main/scala/forcomp/Anagrams.scala
@@ -42,7 +42,7 @@ object Anagrams {
   def stringToChars(string: String): List[Char] = string.toList
 
   /** Converts a sentence into its character occurrence list. */
-  def sentenceOccurrences(s: Sentence): Occurrences = ???
+  def sentenceOccurrences(s: Sentence): Occurrences = wordOccurrences(s.mkString(""))
 
   /** The `dictionaryByOccurrences` is a `Map` from different occurrences to a sequence of all
    *  the words that have that occurrence count.
--- forcomp/src/test/scala/forcomp/AnagramsSuite.scala
+++ forcomp/src/test/scala/forcomp/AnagramsSuite.scala
@@ -55,11 +55,23 @@ class AnagramsSuite extends FunSuite  {
   test("wordOccurrences: Robert") {
     assert(wordOccurrences("Robert") === List(('b', 1), ('e', 1), ('o', 1), ('r', 2), ('t', 1)))
   }
-/*
+  
+  test("sentenceOccurrences: no words") {
+    val result = sentenceOccurrences( List() )
+
+    assert(result === List())
+  }
+
+  test("sentenceOccurrences: single word is just like wordOccurrences of the provided word") {
+    val result = sentenceOccurrences( List("word") )
+
+    assert(result === List(('d', 1), ('o', 1), ('r', 1), ('w', 1)))
+  }
 
   test("sentenceOccurrences: abcd e") {
     assert(sentenceOccurrences(List("abcd", "e")) === List(('a', 1), ('b', 1), ('c', 1), ('d', 1), ('e', 1)))
   }
+/*
 
 
   test("dictionaryByOccurrences.get: eat") {
